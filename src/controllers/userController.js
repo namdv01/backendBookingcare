853a3247d4961b8acd5d9c8d68a956906db2109f
namdv01 <khonghoatay@gmail.com>
2022-08-30 14:50:44 +0700

    test35

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const store = require("../store");
 const cloudinary = require("../cloudinary/cloudinary");

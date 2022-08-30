8354d3da4dd64a666b272c93d09db998b9cc8caf
namdv01 <khonghoatay@gmail.com>
2022-08-30 13:03:40 +0700

    test11

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
 const routes = require("./routes/index");
 const cors = require("cors");
 const connectDB = require("./connectDB");
-const dotenv = require("dotenv").config();
+const db = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-routes(app);
+// routes(app);
 connectDB();
+
+app.get("/testDoctor", async (req, res, next) => {
+  try {
+    const result = await db.User.findAll({
+      where: {
+        roleId: "R2",
+      },
+    });
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 
 const port = process.env.PORT || 5000;
 
